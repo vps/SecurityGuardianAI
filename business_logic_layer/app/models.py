d3caf1bbc2e3c40bd80ceaff34d8b4b7da62d3d7
vps <vps@linkedin.com.se>
2023-05-12 02:14:27 -0500

    Add files via upload

---
+++ business_logic_layer/app/models.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+
+# Models go here
---
+++ presentation_layer/routes.py
@@ -0,0 +1,5 @@
+from flask import render_template, flash, redirect, url_for
+from . import app
+from .forms import LoginForm
+
+# Route definitions here
